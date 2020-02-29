c1bd602eeeaeb3f6411f4bb04bf1e77d4aeace4b
Joe Walton <j.walton.pc@gmail.com>
2020-02-29 17:11:59 +0000

    Add favicons

--- Dockerfile
+++ Dockerfile
@@ -3,11 +3,11 @@ FROM node:12-alpine as deps
 WORKDIR /app
 
 ADD package.json yarn.lock ./
-RUN yarn install --frozen-lockfile --production --network-timeout 10000
+RUN yarn install --production --frozen-lockfile --network-timeout 100000
 
 FROM deps as stage
 
-RUN yarn install --frozen-lockfile --network-timeout 10000
+RUN yarn install --frozen-lockfile --network-timeout 100000
 
 ADD . .
 RUN yarn build
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,17 @@
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 
 import '../styles/global.css';
 import Navigation from '../components/Navigation';
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => (
   <>
+    <Head>
+      <title>Joe Walton | Software Engineer</title>
+      <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="96x96" href="/icons/favicon-96x96.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+    </Head>
     <Navigation />
     <div className="content">
       <Component {...pageProps} />
