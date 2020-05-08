f1c6fe80f86e436de53cbfffce08c8d36c069577
Joe Walton <j.t.walton@icloud.com>
2020-05-08 10:54:03 +0100

    Add netlify cms

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,9 +8,6 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => (
   <>
     <Head>
       <title>Joe Walton | Software Engineer</title>
-      <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="96x96" href="/icons/favicon-96x96.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
     </Head>
     <Navigation />
     <div className="content">
---
+++ src/pages/_document.tsx
@@ -0,0 +1,30 @@
+import NextDocument, { Html, Head, Main, NextScript, DocumentInitialProps, DocumentContext, } from 'next/document';
+
+class Document extends NextDocument {
+
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+    const intialProps = await NextDocument.getInitialProps(ctx)
+    return { ...intialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="96x96" href="/icons/favicon-96x96.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+          <script src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
+          <script src="/scripts/admin-redirect.js" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+};
+
+
+export default Document
